ad71f2a8e71c5a1516f0f16187d22d276c3f91c7
Johny <jasiek.krol.pl@gmail.com>
2024-07-04 13:27:02 +0200

    read file

---
+++ read-file.js
@@ -0,0 +1,13 @@
+const { readFile } = require('fs')
+
+console.log('started a first task')
+// CHECK FILE PATH
+readFile('./first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(result)
+    console.log('complete first task')
+})
+console.log('starting next task')
